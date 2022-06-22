b6f95e2ad8b4cc5648a389fa99e7f4a832498e0b
Tuukka <hartikainentuukka@gmail.com>
2022-06-22 22:12:12 +0300

    updates for backend

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ const { response, request } = require('express')
 const express = require('express')
 const app = express()
 
-app.use(express.json())
-
 let notes = [
     {
         id: 1,
@@ -24,6 +22,19 @@ let notes = [
         important: true
       }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
+
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -51,16 +62,36 @@ app.delete('/api/notes/:id', (request, response) =>{
     response.status(204).end()
 })
 
-app.post('/api/notes', (request, response)=>{
-    const maxId = notes.length > 0
+const generateId = () => {
+  const maxId = notes.length > 0
         ? Math.max(...notes.map(n => n.id))
         : 0
-    const note = request.body
-    
-    note.id = maxId +1
-    notes.concat(note)
+  return maxId +1
+}
+
+app.post('/api/notes', (request, response)=>{
+
+    const body = request.body
+    if(!body.content){
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId(),
+    }
+    notes = notes.concat(note)
     response.json(note)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
